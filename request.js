1da80c5c3bcfb275ed13214669a340e246de1a9c
AdityaNelakonda <aditya.nelakonda@gmail.com>
2017-07-11 14:39:03 -0500

    Update request.js

--- request.js
+++ request.js
@@ -9,15 +9,15 @@ http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
 resp.on('data', function(ip) {
 output.address = ip;
 
-var OSName = "Unknown OS";
-if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
-if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
-if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
-if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+//var OSName = "Unknown OS";
+//if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+//if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+//if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+//if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
 
-output.browser = OSName;
+//output.browser = OSName;
 
-output.language = navigator.language;
+//output.language = navigator.language;
 
 app.get("/", function(res,req)
 {
