f2e67ff8e38b51882a1d40cf01ba00a54508e2d4
AdityaNelakonda <aditya.nelakonda@gmail.com>
2017-07-11 11:38:55 -0500

    Add files via upload

---
+++ request.js
@@ -0,0 +1,37 @@
+
+var express = require("express")
+var http = require("http")
+var app = express()
+var port = process.env.PORT || 3500;
+var output = {address: 0, language : "", browser :""};
+
+http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
+resp.on('data', function(ip) {
+output.address = ip;
+
+var OSName = "Unknown OS";
+if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+
+output.browser = OSName;
+
+output.language = navigator.language
+
+app.get("/", function(res,req)
+{
+    res.send(output)
+
+})
+
+app.listen(port,function()
+{
+  console.log("listening on port 3000")
+})
+});
+});
+
+
+
+//console.log(juice)
