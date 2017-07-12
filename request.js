b9a00acfbd9df19f5e414c72c1707c3117675111
AdityaNelakonda <aditya.nelakonda@gmail.com>
2017-07-11 21:23:17 -0500

    Update request.js

--- request.js
+++ request.js
@@ -2,13 +2,13 @@
 var express = require("express")
 var app = express()
 var port = process.env.PORT || 3500;
-var output = {address: 0, lang : "", browser :""};
+var output = {address: 0, langs : "", browser :""};
 
 
 app.get("/", function(req,res)
 {
     output.address = req.ip;
-    output.lang = req.headers['accept-language'];
+    output.langs = req.headers['accept-language'];
     output.browser = req.headers['user-agent'];
     res.send("Hello");
   
