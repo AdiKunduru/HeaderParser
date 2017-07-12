28b26fe6762cdef8b3e6970a028a801c68ebbd8a
AdityaNelakonda <aditya.nelakonda@gmail.com>
2017-07-11 19:36:39 -0500

    Update request.js

--- request.js
+++ request.js
@@ -1,33 +1,33 @@
 
 var express = require("express")
-//var http = require("http")
+var http = require("http")
 var app = express();
 var port = process.env.PORT || 3500;
-//var output = {address: 0, language : "", browser :""};
+var output = {address: 0, language : "", browser :""};
 
-//http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
-//resp.on('data', function(ip) {
-//output.address = ip;
+http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
+resp.on('data', function(ip) {
+output.address = ip;
 
-//var OSName = "Unknown OS";
-//if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
-//if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
-//if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
-//if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+var OSName = "Unknown OS";
+if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
 
-//output.browser = OSName;
+output.browser = OSName;
 
-//output.language = navigator.language;
+output.language = navigator.language;
 
 app.get("/", function(req,res)
 {
-    res.send("Hello World")
+    res.send(output)
 })
 
 app.listen(port);
     
-//});
-//});
+});
+});
 
 
 
